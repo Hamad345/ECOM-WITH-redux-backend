ab28d60804df9f62360ebd62103ba4a67142d622
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-07-02 16:58:52 +0500

    yes

---
+++ controller/auth-controller/auth-controller.js
@@ -0,0 +1,26 @@
+
+
+
+
+// register
+const register = async(req,res)=>{
+    const {userName,email,password} =req.body;
+    try {
+        
+    } catch (error) {
+        console.log(e)
+        res.status(500).json
+        
+    }
+}
+
+
+
+// login
+
+
+
+// logout
+
+
+// auth middleware
