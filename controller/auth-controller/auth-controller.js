15b735504ef99f3f5aca189ad46e94b502d0ace1
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-07-05 17:33:42 +0500

    yes

--- controller/auth-controller/auth-controller.js
+++ controller/auth-controller/auth-controller.js
@@ -1,15 +1,30 @@
-
+const  bcrypt =require("bcrypt.js")
+const jwt =require("jsonwebtoken")
+const User=require("../../models/User")
 
 
 
 // register
-const register = async(req,res)=>{
+const registerUser = async(req,res)=>{
     const {userName,email,password} =req.body;
     try {
+        const hashPassword =await bcrypt.hash(password,12)
+        const newUser =new User({
+            userName,email,password:hashPassword
+        })
         
+        await newUser.save()
+        res.status(200).json({
+            success:true,
+            message:"Registration succeed"
+        })
     } catch (error) {
         console.log(e)
-        res.status(500).json
+        res.status(500).json({
+            success:false,
+            message: "Some error occured"
+
+        })
         
     }
 }
@@ -23,4 +38,9 @@ const register = async(req,res)=>{
 // logout
 
 
+
 // auth middleware
+
+
+
+module.exports={registerUser}
--- server.js
+++ server.js
@@ -2,9 +2,11 @@
  const mongoose =require('mongoose')
  const cookieParser=require('cookie-parser')
  const cors=require("cors")
+ const authRouter =require("./routes/auth/auth-routes")
 mongoose.connect('mongodb+srv://hamadkhan:hamadKhan654321@cluster0.krc9k.mongodb.net/').then(()=>console.log("connected")).catch((error)=>console.log(error))
  const app=express();
- const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 5000
+ {};
  app.use(
     cors({
         origin : 'http://localhost:5173/',
@@ -21,4 +23,7 @@ mongoose.connect('mongodb+srv://hamadkhan:hamadKhan654321@cluster0.krc9k.mongodb
  )
  app.use(cookieParser());
  app.use(express.json());
+ app.use("/api/auth",authRouter)
+//  /api/auth/registerUser ->registerUser
+//  /api/auth/login ->loginUser
  app.listen(PORT,()=>console.log("server is now running"))
