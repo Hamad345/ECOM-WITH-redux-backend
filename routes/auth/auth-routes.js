e3bc91f7a4623bf77458c0258c7f669cf7c7f09e
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-07-07 22:17:00 +0500

    yes

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -1,7 +1,8 @@
 const express =require("express");
-const {registerUser}=require("../../controller/auth-controller/auth-controller")
+const {registerUser,loginUser}=require("../../controller/auth-controller/auth-controller")
 const router =express.Router();
 
 router.post("/register",registerUser);
+router.post("/register",loginUser);
 
 module.exports = router
