a4e6b4fd721c571e876a8a7f2e12426177830646
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-07-21 22:41:41 +0500

    yes

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -1,8 +1,23 @@
-const express =require("express");
-const {registerUser,loginUser}=require("../../controller/auth-controller/auth-controller")
-const router =express.Router();
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  authMiddleware,
+} = require("../../controllers/auth/auth-controller");
 
-router.post("/register",registerUser);
-router.post("/register",loginUser);
+const router = express.Router();
 
-module.exports = router
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.get("/check-auth", authMiddleware, (req, res) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    user,
+  });
+});
+
+module.exports = router;
